6b0eb4087699c18094c21c48292e4bb6a9b4a86a
Madhaba Patra <madhabapatra@gmail.com>
2025-04-08 22:01:50 +0530

    changed

--- credentials/OpenaiApi.credentials.ts
+++ credentials/OpenaiApi.credentials.ts
@@ -5,7 +5,7 @@ import type {
 	INodeProperties,
 } from 'n8n-workflow';
 
-export class OpenAIApi implements ICredentialType {
+export class OpenaiApi implements ICredentialType {
 	name = 'openAIApi';
 
 	displayName = 'Open AI API';
